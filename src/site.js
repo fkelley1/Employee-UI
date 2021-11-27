1e8ea0ae9abfba3163af2ef5b981bbbaa4d92856
fkelley1 <kelleyf@wit.edu>
2021-11-26 23:36:59 -0500

    Adding initial site functionality, web elements, and style

---
+++ src/site.js
@@ -0,0 +1,279 @@
+//Random Data generated from https://www.json-generator.com/
+//todo move to separate file 
+function getEmployees() {
+	return [
+		{
+			"name": {
+				"firstName": "Herring",
+				"lastName": "Oneal"
+			},
+			"phone": "+1 (905) 461-2560",
+			"bio": "Magna do tempor nulla sunt consectetur sunt incididunt veniam incididunt est adipisicing labore. Irure aute irure ut excepteur cillum irure veniam sunt elit exercitation ullamco magna ad sit. Duis adipisicing velit est duis id proident tempor ipsum qui. Deserunt eiusmod sit proident enim deserunt. Deserunt nostrud eu Lorem cillum mollit dolore ullamco. Ex deserunt veniam minim excepteur officia mollit. Elit Lorem laborum quis eiusmod minim elit.\r\n"
+		},
+		{
+			"name": {
+				"firstName": "Tammy",
+				"lastName": "Booth"
+			},
+			"phone": "+1 (937) 530-3904",
+			"bio": "Fugiat ex non deserunt sunt non proident nostrud dolor id proident dolor aute sunt consequat. Quis aliquip duis qui tempor. Adipisicing nostrud labore aliquip est non eu ad adipisicing ad adipisicing id deserunt eiusmod voluptate. Sint duis fugiat enim reprehenderit voluptate pariatur eiusmod anim qui eu quis. Sunt deserunt incididunt do laboris. Ea consequat deserunt nostrud tempor ipsum magna dolore. Occaecat incididunt sunt culpa sint.\r\n"
+		},
+		{
+			"name": {
+				"firstName": "Sarah",
+				"lastName": "Pierce"
+			},
+			"phone": "+1 (990) 569-3233",
+			"bio": "In consectetur consectetur Lorem exercitation nisi aliqua excepteur non nisi velit ullamco cillum fugiat aliquip. Consectetur dolor aliqua aliquip aliqua est exercitation reprehenderit sit amet. Velit fugiat ea veniam incididunt sit tempor elit nisi.\r\n"
+		},
+		{
+			"name": {
+				"firstName": "Myrna",
+				"lastName": "Vinson"
+			},
+			"phone": "+1 (808) 571-3942",
+			"bio": "Mollit nostrud reprehenderit aute consequat non commodo anim in dolore nostrud commodo aute consectetur. Elit exercitation adipisicing ex in sunt dolore cupidatat. Occaecat Lorem est ut excepteur ea reprehenderit ex labore duis. Quis nulla non nostrud est sint mollit amet anim officia commodo. Sint occaecat ex velit et culpa occaecat commodo.\r\n"
+		},
+		{
+			"name": {
+				"firstName": "Frank",
+				"lastName": "Sykes"
+			},
+			"phone": "+1 (863) 583-3755",
+			"bio": "Pariatur sunt ex dolor nisi adipisicing eiusmod deserunt fugiat et cupidatat dolore sit aliquip elit. Reprehenderit ad eiusmod ipsum eu. Quis adipisicing et exercitation et consequat incididunt ipsum tempor ad occaecat laborum.\r\n"
+		},
+		{
+			"name": {
+				"firstName": "Kathleen",
+				"lastName": "Copeland"
+			},
+			"phone": "+1 (853) 495-2352",
+			"bio": "Commodo non ipsum laboris nisi eiusmod eu nulla non nisi ad. Ut veniam ut ex ex nostrud enim duis eiusmod. Mollit labore anim dolore fugiat amet culpa esse sunt cupidatat ut fugiat fugiat pariatur. Cillum proident ex officia consequat. Exercitation cupidatat amet adipisicing elit esse exercitation aliqua. Exercitation occaecat aute anim ea nostrud laborum pariatur.\r\n"
+		},
+		{
+			"name": {
+				"firstName": "Erica",
+				"lastName": "Cannon"
+			},
+			"phone": "+1 (915) 583-3558",
+			"bio": "Nisi eu laboris minim ea nulla et deserunt et. Amet exercitation sunt irure sint reprehenderit. Ea do enim reprehenderit fugiat aute eu sunt occaecat labore nisi elit eiusmod exercitation ipsum. Aliqua voluptate quis tempor et aliquip qui proident commodo eiusmod eiusmod eiusmod dolor.\r\n"
+		},
+		{
+			"name": {
+				"firstName": "Rosalind",
+				"lastName": "Hughes"
+			},
+			"phone": "+1 (810) 555-2868",
+			"bio": "Pariatur qui esse in ex amet in nostrud sit in nisi consectetur. Excepteur elit in adipisicing Lorem eiusmod tempor officia anim minim id mollit. Ipsum anim qui ad qui consectetur non commodo esse id anim. Pariatur elit velit adipisicing laborum. Id mollit consequat velit tempor.\r\n"
+		},
+		{
+			"name": {
+				"firstName": "Staci",
+				"lastName": "Buchanan"
+			},
+			"phone": "+1 (824) 545-3133",
+			"bio": "Quis fugiat eu incididunt esse deserunt reprehenderit do. Aute incididunt nostrud eu voluptate aliquip enim qui. Ea in cupidatat sunt do anim. Culpa non laborum eiusmod minim in veniam laboris elit cillum velit nisi quis id. Occaecat adipisicing labore id reprehenderit proident. Laboris proident deserunt deserunt aute.\r\n"
+		},
+		{
+			"name": {
+				"firstName": "Hunt",
+				"lastName": "Richmond"
+			},
+			"phone": "+1 (927) 467-3480",
+			"bio": "Magna cillum pariatur sit veniam ipsum proident ad aliquip sunt et nisi ullamco. Fugiat cillum duis consequat occaecat. Eiusmod qui qui enim nisi adipisicing quis. Sunt ea pariatur ullamco sit mollit ut dolore sit qui esse. Irure mollit aliquip sint ut deserunt do consequat laborum mollit culpa labore irure sit. Nulla sit elit fugiat cupidatat. Proident deserunt nulla esse proident.\r\n"
+		},
+		{
+			"name": {
+				"firstName": "Joyner",
+				"lastName": "Berger"
+			},
+			"phone": "+1 (831) 583-2665",
+			"bio": "Exercitation nostrud minim consectetur sunt anim occaecat ea. Adipisicing nulla sit incididunt duis excepteur amet ipsum eiusmod mollit. Dolore mollit eu ullamco sint nisi minim id esse ad id Lorem. Excepteur adipisicing aliquip elit consequat nisi fugiat elit sit cillum. Duis fugiat veniam ea officia laborum.\r\n"
+		},
+		{
+			"name": {
+				"firstName": "Mills",
+				"lastName": "Anthony"
+			},
+			"phone": "+1 (828) 575-2413",
+			"bio": "Anim eu tempor et esse deserunt ad officia cillum ea. Ipsum Lorem Lorem non in consequat amet eiusmod consectetur. Excepteur aute magna voluptate excepteur veniam occaecat aliqua quis dolore excepteur commodo eu do irure. Commodo consequat duis anim magna minim non labore aute. Excepteur exercitation ullamco esse mollit incididunt in minim.\r\n"
+		},
+		{
+			"name": {
+				"firstName": "Marcy",
+				"lastName": "Sherman"
+			},
+			"phone": "+1 (985) 573-3860",
+			"bio": "Reprehenderit tempor culpa sit irure dolore duis elit ut incididunt elit. Cupidatat pariatur reprehenderit mollit id cupidatat dolore laboris culpa id ea ex irure reprehenderit. Eiusmod velit magna magna pariatur deserunt cillum excepteur incididunt qui. Sunt et laborum dolore qui mollit magna exercitation mollit excepteur aute nostrud enim quis. Anim anim quis cupidatat voluptate aliqua. Quis laborum amet consectetur consequat labore reprehenderit id nisi aliqua aute est.\r\n"
+		},
+		{
+			"name": {
+				"firstName": "David",
+				"lastName": "Schneider"
+			},
+			"phone": "+1 (933) 482-2837",
+			"bio": "Culpa nostrud exercitation ut amet eiusmod. Velit eiusmod proident nulla aute tempor laborum nulla excepteur Lorem. Officia veniam magna eu ipsum pariatur laborum est labore ut non duis.\r\n"
+		},
+		{
+			"name": {
+				"firstName": "Figueroa",
+				"lastName": "Valencia"
+			},
+			"phone": "+1 (809) 536-3700",
+			"bio": "Laboris excepteur in incididunt id. Ipsum consequat proident amet ex dolor velit magna commodo veniam ipsum. Ad aute exercitation ullamco officia cillum labore exercitation consectetur enim tempor. Esse fugiat esse ut laborum in excepteur enim velit adipisicing commodo nostrud dolore pariatur laboris. Do adipisicing dolore laborum minim amet ea minim pariatur sint ad irure. Ea nostrud culpa irure ad.\r\n"
+		},
+		{
+			"name": {
+				"firstName": "Hamilton",
+				"lastName": "Ray"
+			},
+			"phone": "+1 (897) 596-3289",
+			"bio": "Amet Lorem minim amet consequat nulla. Quis eu voluptate eiusmod esse voluptate qui ullamco aliqua. Culpa sunt do voluptate nostrud tempor ullamco eiusmod do.\r\n"
+		},
+		{
+			"name": {
+				"firstName": "Chavez",
+				"lastName": "Suarez"
+			},
+			"phone": "+1 (886) 434-3564",
+			"bio": "Reprehenderit sint et ea consequat aute id nostrud ut elit ut. Amet ea labore pariatur non elit sint Lorem dolore magna laboris. Ea ut eu laboris minim ut esse ipsum.\r\n"
+		},
+		{
+			"name": {
+				"firstName": "Carver",
+				"lastName": "Bright"
+			},
+			"phone": "+1 (818) 495-3276",
+			"bio": "Voluptate anim consequat ullamco cupidatat labore. Ut incididunt mollit ut anim deserunt irure. Minim eiusmod sit aliquip magna excepteur laboris. Lorem magna tempor non eu exercitation occaecat aliqua. Non cupidatat in laboris occaecat aute labore excepteur dolore esse culpa fugiat occaecat quis commodo. Duis nulla elit dolor dolor enim velit.\r\n"
+		},
+		{
+			"name": {
+				"firstName": "Maynard",
+				"lastName": "Barnes"
+			},
+			"phone": "+1 (902) 415-2493",
+			"bio": "Pariatur do adipisicing occaecat id ex et in anim aliqua nulla magna. Aliquip aliqua velit nisi velit consequat amet aute adipisicing sit proident. Sit ad occaecat ad dolore fugiat cillum occaecat elit do. Aliquip anim mollit deserunt ut velit officia sit ea officia veniam dolor ipsum. Et voluptate Lorem deserunt qui. Mollit est sint Lorem eu cupidatat do sit irure ex irure do fugiat dolore.\r\n"
+		},
+		{
+			"name": {
+				"firstName": "Gayle",
+				"lastName": "Stewart"
+			},
+			"phone": "+1 (907) 423-2038",
+			"bio": "Eu duis pariatur dolore occaecat nisi deserunt nostrud est cupidatat. Reprehenderit cupidatat est reprehenderit consequat aliqua dolor dolor. Exercitation nisi excepteur velit deserunt tempor ut commodo pariatur irure anim voluptate et. Ut labore nisi et nisi irure tempor aliqua et pariatur consectetur. Nulla reprehenderit eiusmod laborum magna cillum voluptate velit officia id nisi Lorem ea.\r\n"
+		},
+		{
+			"name": {
+				"firstName": "Houston",
+				"lastName": "Kinney"
+			},
+			"phone": "+1 (866) 558-2129",
+			"bio": "Ullamco veniam irure est exercitation ipsum do ex id dolore laborum esse culpa. Eu culpa magna culpa aliqua laborum proident non. Dolore mollit aute eu consequat amet quis. Eu eiusmod ad enim laboris.\r\n"
+		},
+		{
+			"name": {
+				"firstName": "Melisa",
+				"lastName": "Benton"
+			},
+			"phone": "+1 (952) 489-2370",
+			"bio": "Proident veniam proident incididunt occaecat in anim officia enim aliquip consectetur anim deserunt. Adipisicing Lorem voluptate anim fugiat do. Fugiat occaecat reprehenderit aliqua tempor aliquip commodo amet anim consectetur. Lorem culpa proident consequat ex sunt aliqua fugiat laboris. Dolor aliquip laborum officia consectetur culpa minim proident commodo exercitation laborum.\r\n"
+		},
+		{
+			"name": {
+				"firstName": "Hannah",
+				"lastName": "Silva"
+			},
+			"phone": "+1 (808) 492-2960",
+			"bio": "Consequat commodo ullamco esse culpa laboris enim eiusmod cupidatat minim in. Id est proident commodo laboris cupidatat consequat proident non voluptate pariatur elit. Aute id sunt minim consectetur culpa ipsum ullamco aute. Exercitation deserunt veniam ut exercitation culpa velit consequat dolor ipsum sunt laborum ipsum.\r\n"
+		},
+		{
+			"name": {
+				"firstName": "Alvarado",
+				"lastName": "Palmer"
+			},
+			"phone": "+1 (860) 469-2567",
+			"bio": "Exercitation pariatur labore labore eu officia occaecat magna. Pariatur laboris consequat adipisicing incididunt velit et anim reprehenderit qui. Aute cillum exercitation sit ad ipsum quis enim quis in pariatur. Do fugiat est duis nostrud ad irure minim anim ipsum eiusmod nulla esse fugiat. Incididunt quis consequat sit anim consectetur dolor incididunt irure veniam officia esse.\r\n"
+		},
+		{
+			"name": {
+				"firstName": "Kathrine",
+				"lastName": "Clark"
+			},
+			"phone": "+1 (848) 420-2411",
+			"bio": "Eiusmod pariatur officia commodo ipsum deserunt reprehenderit ex id ad ullamco et tempor. Commodo in laboris sit voluptate laboris voluptate. .\r\n"
+		}
+	];
+}
+/**
+ * gets list of JSON objects from getEmployees function
+ * @returns list of employees in html div tags with appropriate classes
+ */
+//TODO Pass employees list to a function to generate the html to avoid repeated code in getAllEmployeeData and search
+function getAllEmployeeData() {
+	let employees = getEmployees();
+	let employeeList = [];
+	let id = 1;
+	for (let employee of employees) {
+		employeeList.push(`<div class="employee-card" id="employee_id_${id}" onClick={showEmployee(this)} tabindex="0"><div>Employee Name: ${employee.name.firstName} ${employee.name.lastName}</div><div>Phone: ${employee.phone}</div><div> Bio: ${employee.bio} </div></div>`);
+		id += 1;
+	}
+	return employeeList.join('');
+}
+/**
+ * search by name
+ */
+//TODO expand search by phone number 
+function search() {
+	let employees = getEmployees();
+	let searchTerm = document.getElementById("txtSearch").value;
+	let searchEmployeeList = [];
+	let id = 1;
+	for (let employee of employees) {
+		let fullName = employee.name.firstName + " " + employee.name.lastName;
+		//case sensitive search
+		if (fullName.includes(searchTerm)) {
+			searchEmployeeList.push(`<div class="employee-card" id="employee_id_${id}" onClick={showEmployee(this)} tabindex="0"><div>Employee Name: ${employee.name.firstName} ${employee.name.lastName}</div><div>Phone: ${employee.phone}</div><div> Bio: ${employee.bio} </div></div>`);
+			id += 1;
+		}
+	}
+	//no return, just set the list generated from search term
+	document.getElementById("employee-list").innerHTML = searchEmployeeList.join('');
+}
+
+/**select specific employee 
+ * show back button, expand card, and remove scroll
+*/
+function showEmployee(row){
+	document.getElementById("txtSearch").style = "display: none;";
+	document.getElementById("employee-list").innerHTML = row.outerHTML;
+	var all = document.getElementsByClassName('employee-card');
+	for (var i = 0; i < all.length; i++) {
+		all[i].style = 'overflow-y: hidden; width: 100%;';
+		
+	}
+	 document.getElementById("btnBack").style = "display: true;";
+	
+
+}
+/**
+ * go back from selected employee
+ */
+function btnBack(){
+	var fullEmployeeList = getAllEmployeeData();
+	document.getElementById("employee-list").innerHTML = fullEmployeeList;
+	document.getElementById("txtSearch").style = "display: true;";
+}
+
+  //TODO fix this --> make enter key open up employee-card 
+	var all = document.getElementsByClassName("employee-card");
+	for (var i = 0; i < all.length; i++) {
+		if (all[i] === document.activeElement) {
+			all[i].addEventListener("keydown", function(event) {
+			if (event.keyCode === 13) {
+			event.preventDefault();
+			showEmployee(all[i]);
+			}
+		});
+	}
+}
+
